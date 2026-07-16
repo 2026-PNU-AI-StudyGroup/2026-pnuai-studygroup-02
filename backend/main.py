99f578f6e41a845fe6e30fd99f34e13032262dcf
nohsieun05 <nohsieun@naver.com>
2026-07-16 22:13:49 +0900

    7/16: Mock 레시피 API 구현

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 # [MAIN-DRAFT] 현지 담당, 이후 계속 확장됨
 from fastapi import FastAPI
+from backend.routers.recipes import router as recipes_router
 
 app = FastAPI()
+app.include_router(recipes_router)
 
 
 @app.get("/")
